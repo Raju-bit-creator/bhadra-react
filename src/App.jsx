8d9ac0646eecdb001c5562a6ba73e5ccb3cc4e07
Raju <rajutamang3456@gmail.com>
2024-10-25 16:02:44 +0545

    alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Alert from './components/Alert'
 
 function App() {
   // const [color, setColor] = useState("blue")
   const [mode , setMode]= useState('light')
   const [text , setText] = useState('enable dark mode')
+  const [alert , setAlert] = useState(null)
+
+  const showAlert =(type, message)=>{
+    setAlert({
+      type: type,
+      message: message
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1000);
+
+  }
+
+
+
    const toggleMode= ()=>{
     if (mode== 'light') {
       setMode('dark')
       setText('enable light mode')
+      showAlert('success','dark mode has been enable')
     }
     else{
       setMode('light')
       setText('enable dark mode')
+      showAlert('success','light mode has been enable')
     }
    }
 
@@ -21,14 +39,7 @@ function App() {
   return (
     <>
     <Navbar title="this is header" mode={mode} text={text} toggleMode= {toggleMode} />
-      {/* <h1>bhadra + group</h1>
-      <div className="card">
-        <button onClick={() => setColor("red")}>
-          please click me {color}
-        </button>
-      
-      </div> */}
-    
+    <Alert alert={alert}  showAlert={showAlert}/>
     </>
   )
 }
