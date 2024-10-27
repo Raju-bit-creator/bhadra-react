a713d56e59a0b63d297e5d3f35d50df6b4982f9a
Raju <rajutamang3456@gmail.com>
2024-10-27 16:26:29 +0545

    changing class to className

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -6,7 +6,7 @@ const Alert = (props) => {
     
     return (
        
-         props.alert &&   <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+         props.alert &&   <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
                 <strong>{props.alert.type}</strong> :{props.alert.message}
                
             </div>
