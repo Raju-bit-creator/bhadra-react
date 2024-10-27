f1a51326ce8786bc263f284ab97445889e14a0f9
Raju <rajutamang3456@gmail.com>
2024-10-27 15:56:20 +0545

    react-router dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import Alert from './components/Alert'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+
+} from "react-router-dom";
+import Contact from './components/Contact';
+import About from './components/About';
+import Home from './components/Home';
 
 function App() {
   // const [color, setColor] = useState("blue")
-  const [mode , setMode]= useState('light')
-  const [text , setText] = useState('enable dark mode')
-  const [alert , setAlert] = useState(null)
+  const [mode, setMode] = useState('light')
+  const [text, setText] = useState('enable dark mode')
+  const [alert, setAlert] = useState(null)
 
-  const showAlert =(type, message)=>{
+  const showAlert = (type, message) => {
     setAlert({
       type: type,
       message: message
@@ -22,24 +31,33 @@ function App() {
 
 
 
-   const toggleMode= ()=>{
-    if (mode== 'light') {
+  const toggleMode = () => {
+    if (mode == 'light') {
       setMode('dark')
       setText('enable light mode')
-      showAlert('success','dark mode has been enable')
+      showAlert('success', 'dark mode has been enable')
     }
-    else{
+    else {
       setMode('light')
       setText('enable dark mode')
-      showAlert('success','light mode has been enable')
+      showAlert('success', 'light mode has been enable')
     }
-   }
+  }
 
 
   return (
     <>
-    <Navbar title="this is header" mode={mode} text={text} toggleMode= {toggleMode} />
-    <Alert alert={alert}  showAlert={showAlert}/>
+      <Router>
+        <Navbar title="this is header" mode={mode} text={text} toggleMode={toggleMode} />
+        <Alert alert={alert} showAlert={showAlert} />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/blogs' element={<Contact />} />
+            <Route path='/about' element={<About />} />
+
+          </Routes>     
+
+      </Router>
     </>
   )
 }
