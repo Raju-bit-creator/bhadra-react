8b19adbbf000165836e613a8fd3eb66f7d24d7e6
Raju <rajutamang3456@gmail.com>
2024-11-10 16:11:02 +0545

    working with json object

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,17 +2,40 @@ import React, { useContext, useEffect } from 'react'
 import productContext from '../context/productContext'
 
 const About = () => {
-    const context =useContext(productContext)
-    const {product , update}= context
- 
-    useEffect(()=>{
-      update()
-    },[])
+    const context = useContext(productContext)
+    const { product, update, news } = context
+    console.log("this is my news ", news);
+
+
+    useEffect(() => {
+        update()
+    }, [])
 
     return (
-        <div>
-            <h4> this is about us component. my product name is: {product.name} and price:{product.price}</h4>
-        </div>
+        <>
+            <div className="container">
+                <div className="row">
+                    {/* <h4> this is about us component. my product name is: {product.name} and price:{product.price}</h4> */}
+                    <h4 className="news-header">
+                        My News
+                    </h4>
+                    {news &&news.map((item)=>{
+                        return(
+                            <div key={item.source.id} className="card col-md-3">
+                            <img src="..." className="card-img-top" alt="..."/>
+                                <div className="card-body">
+                                    <h5 className="card-title">{item.author}</h5>
+                                    <p className="card-text">{item.description}</p>
+                                    <a href={item.url} target="blank" className="btn btn-primary">Go somewhere</a>
+                                </div>
+                        </div>
+                        )
+                       
+                    })}
+
+                </div>
+            </div>
+        </>
     )
 }
 
