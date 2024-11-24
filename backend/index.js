a76063ffe472b86d21212f83e7680d00045aa827
Raju <rajutamang3456@gmail.com>
2024-11-24 15:58:25 +0545

    authentication

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,11 @@ const dbConnect = require('./db')
 const app = express()
 const port=5000
 dbConnect()
+app.use(express.json())
 
 
 
-app.use('auth/api',require('./routes/Auth'))
+app.use('/api/auth', require('./routes/Auth'))
 
 app.listen(port,()=>{
     console.log(`server is running on http://localhost:${port}`);
