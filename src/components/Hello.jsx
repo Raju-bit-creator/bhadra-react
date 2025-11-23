a420595bad454ff402a9027e5722d659c8c9b8b1
Raju Tamang <rajutamang3456@gmail.com>
2025-11-23 21:18:07 +0545

    code rabbit

---
+++ src/components/Hello.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+const Hello = () => {
+    const [count, setCount]= useState(0)   
+    const [name, setName]= useState("ramesh")   
+
+    const handleClick = ()=>{
+        setCount(prev => prev + 1)
+
+    }
+
+consoie.log("hello component rendered");
+
+  return (
+    <div>
+      <button onClick={handleClick}>Click me</button>
+      <p>Clicked {count} {count === 1 ? 'time' : 'times'}</p>
+
+      <input 
+        type="text" 
+        value={name} 
+        onChange={(e) => setName(e.target.value)} 
+      />
+      <p>Your name is: {name}</p>
+    </div>
+  )
+}
+
+export default Hello
